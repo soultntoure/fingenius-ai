cd8dea68232050c5131c50f27f6748c19ea52361
Toure <142293316+soultntoure@users.noreply.github.com>
2025-06-27 12:21:15 +0800

    feat: scaffold backend/src/models/auth.py

---
+++ backend/src/models/auth.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel, EmailStr
+from typing import Optional
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    email: Optional[str] = None
+
+class LoginSchema(BaseModel):
+    username: str # Can also be email
+    password: str
